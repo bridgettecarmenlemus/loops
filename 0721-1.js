f744689135ddd96214884a9687e0cba59e0a8cb5
Bridgette Lemus <bridgettelemus@Bridgettes-MacBook-Pro.local>
2022-07-22 08:49:48 -0400

    functions hw

---
+++ 0721-1.js
@@ -0,0 +1,15 @@
+// Create a function that returns a base-2 (binary) representation of a base-10 (decimal) string number. To convert is simple: ((2) means base-2 and (10) means base-10) 010101001(2) = 1 + 8 + 32 + 128.
+
+// Going from right to left, the value of the most right bit is 1, now from that every bit to the left will be x2 the value, value of an 8 bit binary numbers are (256, 128, 64, 32, 16, 8, 4, 2, 1).
+
+
+// function binary(decimal){
+//     return decimal.toString(2);
+// }
+
+
+// console.log(binary(.5))
+function binary(decimal) {
+	return decimal.toString(2);
+}
+console.log(binary(2.2))
